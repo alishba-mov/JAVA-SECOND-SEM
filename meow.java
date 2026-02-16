b2f83476fc7d7698231f9d2d2cfa9b552441fa87
alishba-mov <adnanhanif823@gmail.com>
2026-02-16 12:46:58 +0500

    Add files via upload

---
+++ meow.java
@@ -0,0 +1,17 @@
+class RectangleArea {
+    public static void main(String[] args) {
+
+        int length = 10;
+        int width = 5;
+
+        int area = length * width; 
+
+        // Displaying result
+        System.out.println("Length: " + length);
+        System.out.println("Width: " + width);
+        System.out.println("Area of rectangle: " + area);
+    }
+}
+
+
+
