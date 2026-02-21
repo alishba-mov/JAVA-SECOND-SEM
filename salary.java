bded673f8a3c7bc325f742a42f08e8de498bb051
alishba-mov <adnanhanif823@gmail.com>
2026-02-22 01:28:42 +0500

    Add files via upload

---
+++ salary.java
@@ -0,0 +1,11 @@
+public class salary {
+
+    public static void main(String[] args){
+        long sal = 50000;
+        System.out.println("Salary: "+sal);
+        for (int i=1; i<=5; i++){
+            System.out.println("After "+i+" Years: " +(sal+i*(2000)));
+    
+        }
+    }
+}
