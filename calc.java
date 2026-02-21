b7d081f55ddff97f4521b535f0c3b8548871002a
alishba-mov <adnanhanif823@gmail.com>
2026-02-22 01:17:48 +0500

    Add files via upload

---
+++ calc.java
@@ -0,0 +1,17 @@
+public class calc {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 5;
+
+        int add = num1 + num2;
+        int sub = num1 - num2;
+        int mul = num1 * num2;
+        int div = num1 / num2;
+
+        System.out.println("Addition = " + add);
+        System.out.println("Subtraction = " + sub);
+        System.out.println("Multiplication = " + mul);
+        System.out.println("Division = " + div);
+    }
+}
