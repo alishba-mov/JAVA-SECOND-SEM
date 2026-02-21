d06a6b564d56a88861fff80495d71c21b2628223
alishba-mov <adnanhanif823@gmail.com>
2026-02-22 01:38:07 +0500

    Add files via upload

---
+++ friend.java
@@ -0,0 +1,12 @@
+public class friend {
+    public static void main(String[] args) {
+
+        String name = "Areeba";
+        String tagline = "Drama queen but loyal";
+        String faces = "😂 😒 🤡 😭";
+
+        System.out.println("Friend Name: " + name);
+        System.out.println("Tagline: " + tagline);
+        System.out.println("Favorite Faces: " + faces);
+    }
+}
